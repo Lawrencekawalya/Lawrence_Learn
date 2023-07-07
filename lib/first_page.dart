fb6d8ed6be3f6791bb3148ec9436085bd19a2dc5
Lawrence <kawalya.lawrence2016@gmail.com>
2023-07-07 16:49:43 +0300

    add first_page and second_page

---
+++ lib/first_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MyFirstPage extends StatelessWidget {
+  const MyFirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Internship APP"),
+          elevation: 20.0,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20.0),
+              const CircleAvatar(
+                  radius: 40.0,
+                  // child: Text("BSU"),
+                  backgroundImage: AssetImage("images/profile.jpeg")),
+              const SizedBox(height: 10.0),
+              const Text(
+                "Students of BSU",
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {
+                        if (kDebugMode) {
+                          print("I was pressed");
+                        }
+                      },
+                      child: const Text("Thanks you"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    OutlinedButton(
+                      onPressed: () {
+                        if (kDebugMode) {
+                          print("I was pressed");
+                        }
+                      },
+                      child: const Text("Thanks you"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    OutlinedButton(
+                      onPressed: () {
+                        if (kDebugMode) {
+                          print("I was pressed");
+                        }
+                      },
+                      child: const Text("Thanks you"),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'first_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,69 +24,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyFirstPage extends StatelessWidget {
-  const MyFirstPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Internship APP"),
-          elevation: 20.0,
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20.0),
-              const CircleAvatar(
-                  radius: 40.0,
-                  // child: Text("BSU"),
-                  backgroundImage: AssetImage("images/profile.jpeg")),
-              const SizedBox(height: 10.0),
-              const Text(
-                "Students of BSU",
-                style: TextStyle(
-                  color: Colors.pink,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    OutlinedButton(
-                      onPressed: () {
-                        if (kDebugMode) {
-                          print("I was pressed");
-                        }
-                      },
-                      child: const Text("Thanks you"),
-                    ),
-                    const SizedBox(width: 10.0),
-                    OutlinedButton(
-                      onPressed: () {
-                        if (kDebugMode) {
-                          print("I was pressed");
-                        }
-                      },
-                      child: const Text("Thanks you"),
-                    ),
-                    const SizedBox(width: 10.0),
-                    OutlinedButton(
-                      onPressed: () {
-                        if (kDebugMode) {
-                          print("I was pressed");
-                        }
-                      },
-                      child: const Text("Thanks you"),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
-  }
-}
